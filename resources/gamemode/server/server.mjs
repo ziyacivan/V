b9c9b2212b4da47f2277cc510796c187e4716371
Rubiaceae <ziyacivan@yahoo.com>
2021-01-28 02:40:24 +0300

    Added /giveweapon command with weapon array list.
    Weapon array is have 2 weapons for now.

--- resources/gamemode/server/server.mjs
+++ resources/gamemode/server/server.mjs
@@ -1,5 +1,6 @@
 import * as alt from 'alt';
 import * as chat from 'chat';
+import { weaponList } from './weapons.mjs';
 
 const playerSpawn = {
     x: 1070.206,
@@ -10,12 +11,24 @@ const playerSpawn = {
 alt.on('playerConnect', (player) => {
     player.model = 'a_m_y_beachvesp_01';
     player.spawn(playerSpawn.x, playerSpawn.y, playerSpawn.z, 0);
+
+    alt.log(`${player.name} has joined with the ID of ${player.id}`);
 });
 
 alt.on('playerDeath', (target, killer, weapon) => {
     player.spawn(playerSpawn.x, playerSpawn.y, playerSpawn.z, 0);
 });
 
+// /giveweapon [weapon_name]
 chat.registerCmd('giveweapon', (player, args) => {
-    
+    if (args.length <= 0)
+        return chat.send(player, "{FF6347}/giveweapon [weapon_name]");
+
+    const weaponName = args[0].toLowerCase();
+
+    if (weaponList[weaponName] === undefined)
+        return chat.send(player, '{FF6347} Weapon type is not valid.');
+
+    player.giveWeapon(weaponList[weaponName], 999, true);
+    chat.send(player, 'You were given a weapon.');
 });
---
+++ resources/gamemode/server/weapons.mjs
@@ -0,0 +1,4 @@
+export const weaponList = {
+    'unarmed': -1569615261,
+    'knife': -1716189206
+};
